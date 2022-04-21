bacc110712ec10dfe07c655f6dcfa97acf3022ee
Kimura-Taiki <taiguimucum850@gmail.com>
2022-04-21 08:08:08 +0000

    3.2.3まで

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,6 +1,13 @@
 class ChatChannel < ApplicationCable::Channel
+  rescue_from "MyError", with: :deliver_error_message
+    
   # コンシューマーがこのチャンネルのサブスクライバーになると
   # このコードが呼び出される
   def subscribed
+  end
+  
+  private
+  def deliver_error_message(e)
+    broadcast_to("...")
   end
 end
