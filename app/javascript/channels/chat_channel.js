2c05ef70156550833982fdcdd9fa9fbbd30d13fc
Kimura-Taiki <taiguimucum850@gmail.com>
2022-04-21 08:51:42 +0000

    5.3まで

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -1,6 +1,25 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "ChatChannel", room: "BestRoom" })
+consumer.subscriptions.create({ channel: "ChatChannel", room: "BestRoom" }, {
+  received(data) {
+    this.appendLine(data)
+  },
+  
+  appendLine(data) {
+    const html = this.createLine(data)
+    const element = document.querySelector("[data-chat-room='BestRoom']")
+    element.insertAdjacentHTML("beforeend", html)
+  },
+  
+  createLine(data) {
+    return `
+      <article class="chat-line">
+        <span class="speaker">${data["send_by"]}</span>
+        <span class="body">${data["body"]}</span>
+      </article>
+    `
+  }
+})
 
 // consumer.subscriptions.create({ channel: "ChatChannel", room: "1stRoom" })
 // consumer.subscriptions.create({ channel: "ChatChannel", room: "2ndRoom" })
